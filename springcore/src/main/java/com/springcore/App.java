c4f6a35ea4c2f85195688b3e95cb44ddbb4ee89e
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2022-11-13 21:40:10 +0530

    Adding comment

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -4,14 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ *Class to create ApplicationContext object and getting bean object
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         	Student stud = (Student)context.getBean("student1");
         	System.out.println(stud);
